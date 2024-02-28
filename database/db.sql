3f5c927ab013046b87d726800fef716938c80126
leonfuns <107266694+leonfuns@users.noreply.github.com>
2024-02-29 00:40:51 +0800

    Update database

--- database/db.sql
+++ database/db.sql
@@ -1,9 +1,14 @@
--- FinSense
+/*
+ * Name:	FinSense
+ * Version: 0.1.0
+ * Update:	28/02/2024
+ * Author:	Zoe
+ */
+
 --  *****************************************************************************************
 
 --	Create lzmdb database if it does not exist.
 DROP DATABASE IF EXISTS lzmdb;
-
 CREATE DATABASE lzmdb;
 USE lzmdb;
 
@@ -12,103 +17,151 @@ USE lzmdb;
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users(
-	userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(100) NOT NULL, CHECK (email LIKE '%@%'),
-	password VARCHAR(20) NOT NULL,
-	title VARCHAR(12) NULL, CHECK (title IN ('Mr', 'Miss', 'Ms', 'Mrs')),
-	firstName	VARCHAR(20) NOT NULL,
-	lastName VARCHAR(20) NOT NULL,
-	mobile CHAR(10) NOT NULL, CHECK (LENGTH(mobile) = 10),
-	addrStreet VARCHAR(50) NULL,
-	addrCity VARCHAR(15) NULL,
-	addrPostcode CHAR(4) NULL, CHECK (LENGTH(addrPostcode) = 4),
-    createDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
+	userID			INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email			VARCHAR(100)	NOT NULL, 
+		CHECK (email LIKE '%@%'),
+	password		VARCHAR(20)		NOT NULL,
+	title			VARCHAR(12)		NULL, 
+		CHECK (title IN (
+						'Mr', 
+						'Miss', 
+						'Ms', 
+						'Mrs'
+						)), -- DEFAULT IS NULL but CHECK IS NOT incluted NULL, so we cannot enter NULL.
+	firstName		VARCHAR(20)		NOT NULL,
+	lastName		VARCHAR(20)		NOT NULL,
+	mobile			CHAR(10)		NOT NULL, 
+		CHECK (LENGTH(mobile) = 10),
+	addrStreet		VARCHAR(50)		NULL,
+	addrCity		VARCHAR(15)		NULL,
+	addrPostcode	CHAR(4)			NULL, 
+		CHECK (LENGTH(addrPostcode) = 4), -- Defult IS NULL but CHECK LENGTH TO 4
+    createDate		DATETIME		NOT NULL DEFAULT CURRENT_TIMESTAMP);
 
+   
 -- Create Income table for storing income information.
 DROP TABLE IF EXISTS Income;
 
 CREATE TABLE Income(
-	incomeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userID INT NOT NULL,
-	company VARCHAR(200) NULL,
-	industry VARCHAR(100) NULL,
-	jobType	VARCHAR(15) NULL,
-		CHECK (jobType IN ('FullTime', 'PartTime', 'Casual', 'OtherIncome', NULL)),
-	position VARCHAR(50) NULL,
-	location VARCHAR(200) NULL,
-	startDate DATETIME NOT NULL,
-	endDate DATETIME NULL,
-	frequency VARCHAR(15) NOT NULL, CHECK (frequency
-		IN ('OneTime','Weekly','Fornightly','Monthly','Quarterly','Seminannually','Annual')),
-    status BIT NOT NULL  -- binary, use 0 for on, 1 for off
-    ) AUTO_INCREMENT = 101;
+	incomeID	INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID		INT				NOT NULL, -- NO fk, it will allow ANY userID even it's NOT EXISTS.
+	company		VARCHAR(200)	NULL,
+	industry	VARCHAR(100)	NULL,
+	jobType		VARCHAR(15)		NULL,
+		CHECK (jobType IN (
+							'FullTime', 
+							'PartTime', 
+							'Casual', 
+							'OtherIncome', 
+							NULL
+							)),
+	POSITION	VARCHAR(50)		NULL,
+	location	VARCHAR(200)	NULL,
+	startDate	DATETIME		NOT NULL, -- Maybe we can consider the data type as DATE, only save the date without time.
+	endDate		DATETIME		NULL, -- (Leon note: Check with status.) Ignore my note, and I'll be back to look next time.
+	frequency	VARCHAR(15)		NOT NULL, 
+		CHECK (frequency IN (
+							'OneTime',
+							'Weekly',
+							'Fornightly',
+							'Monthly',
+							'Quarterly',
+							'Seminannually',
+							'Annual'
+							)),
+    status		BIT				NOT NULL  -- binary, use 0 for on, 1 for off
+    ) AUTO_INCREMENT = 101; -- Double auto_increment
 
+    
 -- Create Salaries table for storing income breakdown.
 DROP TABLE IF EXISTS Salaries;
 
 CREATE TABLE Salaries(
-	salaryID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	incomeID INT NOT NULL,
-	amount INT NOT NULL,
-    salaryDate DATETIME NOT NULL,
-	note TEXT NULL) AUTO_INCREMENT = 1001;
+	salaryID	INT			NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	incomeID	INT			NOT NULL, -- NO fk
+	amount		INT			NOT NULL,
+    salaryDate	DATETIME	NOT NULL, -- Maybe we can consider the data type as DATE, only save the date without time.
+	note		TEXT		NULL) AUTO_INCREMENT = 1001; -- Double auto_increment
+
 
 -- Create Categories table for the categories of spendings and goals.
 DROP TABLE IF EXISTS Categories;
 
 CREATE TABLE Categories(
-	categoryID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	categoryName VARCHAR(50) NOT NULL
-		CHECK (categoryName IN ('Food', 'Education', 'Entertainment', 'Groceries',
-		  'Health', 'Home', 'Shopping', 'SuperContributions', 'TaxPaid','Travel',
-		  'Transfers', 'Transport', 'Utilities', 'Uncategorised'))) AUTO_INCREMENT = 3001;
+	categoryID		INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	categoryName	VARCHAR(50) NOT NULL -- The longest word IS 18 IN your CHECK.
+		CHECK (categoryName IN (
+								'Food', 
+								'Education', 
+								'Entertainment', 
+								'Groceries',
+								'Health', 
+								'Home', 
+								'Shopping', 
+								'SuperContributions', 
+								'TaxPaid',
+								'Travel',
+								'Transfers', 
+								'Transport', 
+								'Utilities', 
+								'Uncategorised' -- We have an uncategorised label FOR the USER; consider setting it as default if the user forgets to enter something.
+								))) AUTO_INCREMENT = 3001; -- Double auto_increment
 
+		 
 -- 	Create Spendings for storing user spending details.
 DROP TABLE IF EXISTS Spendings;
 
 CREATE TABLE Spendings(
-	spendingID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userID INT NOT NULL,
-	categoryID INT NOT NULL,
-	spDateTime DATETIME	NOT NULL,
-	amount INT NOT NULL,
-	note TEXT NULL) AUTO_INCREMENT = 9001;
+	spendingID	INT			NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID		INT			NOT NULL, -- NO fk
+	categoryID	INT			NOT NULL, -- NO fk
+	spDateTime	DATETIME	NOT NULL, -- Maybe we can consider the data type as DATE, only save the date without time.
+	amount		INT			NOT NULL,
+	note		TEXT		NULL) AUTO_INCREMENT = 9001; -- Double auto_increment
+
 
 -- 	Create Goals for storing the goals of users.
 DROP TABLE IF EXISTS Goals;
 
 CREATE TABLE Goals(
-	goalID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userID INT NOT NULL,
-	categoryID INT NOT NULL,
-	gName VARCHAR(100) NOT NULL,
-	startDate DATETIME NOT NULL,
-	finalDate DATETIME NOT NULL,
-	amount INT NOT NULL,
-	note TEXT NULL) AUTO_INCREMENT = 4001;
+	goalID		INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID		INT				NOT NULL, -- NO fk
+	categoryID	INT				NOT NULL, -- NO fk
+	gName		VARCHAR(100)	NOT NULL,
+	startDate	DATETIME		NOT NULL, -- Maybe we can consider the data type as DATE, only save the date without time.
+	finalDate	DATETIME		NOT NULL,
+	amount		INT				NOT NULL,
+	note		TEXT			NULL) AUTO_INCREMENT = 4001; -- Double auto_increment
+
 
 -- Create Investment Types table for the investments.
 DROP TABLE IF EXISTS InvestType;
 
 CREATE TABLE InvestType(
-	investTypeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	investType VARCHAR(50) NOT NULL, CHECK (investType
-		IN ('Stock', 'Bond','Crypto', 'OtherInvestment'))
-    ) AUTO_INCREMENT = 5001;
+	investTypeID	INT				NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	investType		VARCHAR(50)		NOT NULL, 
+		CHECK (investType IN (
+								'Stock', 
+								'Bond',
+								'Crypto', 
+								'OtherInvestment'
+								)) -- the longest word is 15
+    ) AUTO_INCREMENT = 5001; -- Double auto_increment
 
+   
 -- Create Investments table for recording investments.
 DROP TABLE IF EXISTS Investments;
 
 CREATE TABLE Investments(
-	investmentID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userID INT NOT NULL,
-	categoryID INT NOT NULL,
-	investAmount INT NOT NULL,
-	startDate DATETIME NOT NULL,
-	maturityDate DATETIME NULL,
-	marketValue INT NULL,
-	investReturn INT NOT NULL DEFAULT 0,
-	  CHECK (investReturn >= 0 AND investReturn <= 100)) AUTO_INCREMENT = 6001;
+	investmentID	INT			NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID			INT			NOT NULL, -- NO fk
+	categoryID		INT			NOT NULL, -- NO fk
+	investAmount	INT			NOT NULL,
+	startDate		DATETIME	NOT NULL, -- Maybe we can consider the data type as DATE, only save the date without time.
+	maturityDate	DATETIME	NULL,
+	marketValue		INT			NULL,
+	investReturn	INT			NOT NULL DEFAULT 0,
+	  CHECK (investReturn >= 0 AND investReturn <= 100)) AUTO_INCREMENT = 6001; -- Double auto_increment
 
 --	**************************************************************************************
 --	Insert data into database.
