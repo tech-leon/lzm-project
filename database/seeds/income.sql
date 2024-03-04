32b17fb95080c33727c016cc33ffd2f691fbd655
Zoechen0202 <159683888+Zoechen0202@users.noreply.github.com>
2024-03-04 20:10:08 +0800

    seeds

---
+++ database/seeds/income.sql
@@ -0,0 +1,23 @@
+INSERT INTO Income (userID, company, industry, jobType, position, location, startDate, endDate, frequency, status) 
+VALUES 
+    ('1', 'BHP Group', 'Mining', 'FullTime', 'Engineer', 'Perth', '2024-03-02', '2027-01-18', 'Weekly', 0),
+    ('1', NULL, NULL, 'PartTime', NULL, 'Perth', '2019-08-09', '2022-07-24', 'Weekly', 1),
+    ('2', 'Commonwealth Bank', 'Banking/Finance', 'FullTime', 'Financial Analyst', 'Perth', '2020-05-29', '2023-05-14', 'Weekly', 1),
+    ('2', 'Telstra Corporation', 'Telecommunications', 'PartTime', 'Network Technician', 'Perth', '2023-11-05', '2026-10-20', 'Monthly', 0),
+    ('2', NULL, 'Banking/Finance', 'PartTime', 'Bank Teller', 'Perth', '2021-08-20', '2024-08-04', 'Semiannually', 0),
+    ('3', 'Woolworths Group', 'Retail', 'FullTime', 'Store Manager', 'Perth', '2019-04-26', '2022-04-10', 'Weekly', 1),
+    ('3', 'Wesfarmers', 'Conglomerate/Retail', 'Casual', 'Sales Associate', 'Perth', '2021-08-04', '2024-07-19', 'Quarterly', 0),
+    ('4', 'CSL Limited', 'Biotechnology/Health', 'FullTime', 'Research Scientist', 'Sydney', '2021-05-01', '2024-04-15', 'Weekly', 0),
+    ('5', 'Rio Tinto', 'Mining', 'PartTime', 'Operator', 'Perth', '2023-12-01', '2026-11-15', 'Monthly', 0),
+    ('5', 'Newcrest Mining', 'Mining', 'OtherIncome', 'Geologist', 'Perth', '2022-08-22', NULL, 'OneTime', 1),
+    ('6', 'Macquarie Group', 'Banking/Finance', 'FullTime', 'Investment Analyst', 'Perth', '2021-05-18', '2024-05-02', 'Monthly', 0),
+    ('7', 'Woodside Petroleum', 'Oil & Gas', 'PartTime', 'Technician', 'Brisbane', '2023-11-20', '2026-11-04', 'Quarterly', 0),
+    ('7', 'AMP Limited', 'Financial Services', 'OtherIncome', 'Customer Relationship', 'Brisbane', '2019-07-06', '2022-06-20', 'Monthly', 1),
+    ('8', 'Qantas Airways', 'Aviation','Casual', 'Flight Attendant', NULL, '2020-02-23', '2023-02-07', 'Annual', 1),
+    ('8', 'Fortescue Metals Group', 'Mining', 'Casual', 'Truck Driver', 'Perth', '2021-12-10', '2024-11-24', 'Weekly', 0),
+    ('8', 'Transurban Group', NULL, NULL, 'Traffic Controller', 'Perth', '2022-03-23', NULL, 'Fornightly', 0),
+    ('9', 'Cochlear Limited', 'Medical Technology', 'FullTime', 'Biomedical Engineer', 'Melbourne', '2022-11-04', '2025-10-19', 'Annual', 0),
+    ('9', 'Sonic Healthcare', 'Healthcare Services', 'Casual', NULL, NULL, '2022-06-19', NULL, 'OneTime', 1),
+    ('10', 'Origin Energy', 'Energy', NULL, NULL, NULL, '2024-01-25', NULL, 'OneTime', 0),
+    ('10', 'Suncorp Group', 'Insurance/Banking', NULL, 'Claims Officer', 'Perth', '2020-08-10', '2023-07-26', 'Fornightly', 1);
+
--- database/seeds/users.sql
+++ database/seeds/users.sql
@@ -1,2 +1,11 @@
-
-ygyugguygyugu
+INSERT INTO `Users` (email, password, title, firstName, lastName, mobile, addrStreet, addrCity, addrPostcode) VALUES 
+    ('E.Smith@Main.com', 'Password1!', 'Ms', 'Emily', 'Smith', '0412345678', '123 Main St', 'Perth', '6000'),
+	('C.Green@Hay.com', 'Green123$', 'Miss', 'Chloe', 'Green', '0434567891', '789 Hay St', 'Perth', '6000'),
+	('E.Lee@Oxfo.com', 'LeeRecep12!', 'Mr', 'Ethan', 'Lee', '0467891234', '321 Oxford St', 'Perth', '6000'),
+	('W.Johnson@Geor.com', 'Password2!', 'Mr','William', 'Johnson', '0423456789', '456 George St', 'Sydney', '2000'),
+	('J.Kim@Hay.com', 'KimPass12#', 'Mr', 'James', 'Kim', '0489123456', '876 Hay St', 'Perth', '6000'),
+	('O.Jackson@Quee.com', 'Jackson789*', 'Mrs', 'Olivia', 'Jackson', '0456789123', '789 Queen St', 'Perth', '6000'),
+	('W.Nguyen@Murr.com', 'Nguyen123$', 'Mr', 'William', 'Nguyen', '0412345678', '123 Murray St', 'Brisbane', '4000'),
+	('S.Liu@Well.com', 'LiuRecep34@', 'Mrs', 'Sophia', 'Liu', '0491234567', '987 Wellington St', 'Perth', '6000'),
+	('D.Taylor@King.com', 'Taylor456#', 'Mr', 'Daniel', 'Taylor', '0445678912', '456 King St', 'Melbourne', '3000'),
+	('G.Zhu@Geor.com', 'ZhuRecep34$', NULL, 'Grace', 'Zhu', '0478912345', '654 George St', 'Perth', '6000');
